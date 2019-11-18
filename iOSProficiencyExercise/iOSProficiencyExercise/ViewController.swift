cf53f482b99b90f016f34c0cad4712a56cb439d5
Vaneet Modgill <vaneet.modgill@sdnatech.com>
2019-11-18 23:48:21 +0530

    Cocoapods Set up
    Added dependencies like alamofire, kingfisher and json decoder

---
+++ iOSProficiencyExercise/iOSProficiencyExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSProficiencyExercise
+//
+//  Created by Vaneet Modgill on 18/11/19.
+//  Copyright © 2019 Jasmeet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
